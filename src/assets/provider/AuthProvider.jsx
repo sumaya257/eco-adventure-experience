d61dda57a25ca2a3f847be6224f4cf0f524077d6
sumaya257 <sumayaece19@gmai.com>
2024-11-20 14:32:20 +0600

    login-and-registration-page-added

--- src/assets/provider/AuthProvider.jsx
+++ src/assets/provider/AuthProvider.jsx
@@ -1,12 +1,22 @@
 import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+
 export const AuthContext = createContext()
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null)
 
+    const createNewUser=(email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
     const authInfo={
         user,
         setUser,
+        createNewUser
     }
     return (
        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +22,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
 export default app
